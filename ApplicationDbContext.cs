f1b6518cee4e4d6bc96185665e1f808c052fc673
Stol_in54 <Stol_in54@student.itstep.org>
2023-04-11 20:41:05 +0300

    Добавьте файлы проекта.

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore; 
+
+namespace WebApplication8
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        { 
+        }
+        public DbSet<Task> tasks { get; set; } 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-57UKIQP;Initial Catalog=tasks21;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
